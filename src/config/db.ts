6502aaa1e48ad084a156f580424ab590af311306
mmanueljoe <emmanuel.letsu@amalitech.com>
2026-05-20 13:29:13 +0000

    chore: update packages to include pino-pretty

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ import { config, logger } from "./index.js";
 export const connectDB = async () => {
   try {
     await mongoose.connect(config.mongoUri);
-    logger.info("Connected to MongoDB");
+    logger.info("Connected to database successfully");
   } catch (err) {
     logger.fatal({ err }, "Failed to connect to database");
     process.exit(1);
